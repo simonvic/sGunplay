ddabbf3571e85d9c672ea9f72589c2624cfbd2ae
simonvic <simonvic.dev@gmail.com>
2021-05-19 14:40:14 +0200

    Improved aiming model

--- scripts/3_Game/sConstants.c
+++ scripts/3_Game/sConstants.c
@@ -26,22 +26,29 @@ class GunplayConstants{
 	
 	/////////////////////////////////////////////////////////////
 	// AIMING MODEL
-	static const bool USE_LEGACY_MODE = true;               // Use legacy mode (0.62 features)
-
-	static const float RESET_SPEED_INSPECTION = 0.1;        // how fast the camera will reset from inspection angles to default. 0.1 = fast | 0.4 = slow
-	static const float RESET_SPEED_FREELOOK = 0.1;          // how fast the camera will reset from freelook angles to default. 0.1 = fast | 0.4 = slow
-	static const float RESET_SPEED_DEADZONE = 0.3;          // how fast the camera will reset from deadzone angles to default. 0.1 = fast | 0.4 = slow
-	static const float RESET_SPEED_DEADZONE_WEAPON = 0.3;   // how fast the camera will reset from weapon deadzone angles to default. 0.1 = fast | 0.4 = slow
+	static const bool USE_WEAPON_INERTIA = true;              // Use weapon inertia in the player aiming model
+	static const bool CAMERA_FOLLOWS_BREATHING_SWAY = false;  // Camera will move along the breathing sway
 	
-	static const float INERTIA_SMOOTHNESS = 0.2;            // how smooths the hands will "lag" behind the camera. Recommended 0.15 > x > 0.40
-	static const float INERTIA_MULTIPLIER_BASE = 5;         // Base multiplier of the amount of inertia to be applied
-	static const float INERTIA_MULTIPLIER_HIPFIRE = 5;      // Multiplier of the amount of inertia to be applied when the player is in hipfire
-	static const float INERTIA_MULTIPLIER_ERECT = 1;        // Multiplier of the amount of inertia to be applied when the player is ERECT
-	static const float INERTIA_MULTIPLIER_CROUCHED = 0.35;  // Multiplier of the amount of inertia to be applied when the player is CROUCHED
-	static const float INERTIA_MULTIPLIER_PRONE = 0.01;     // Multiplier of the amount of inertia to be applied when the player is PRONE
+	static const float RESET_SPEED_INSPECTION = 0.1;          // how fast the camera will reset from inspection angles to default. 0.1 = fast | 0.4 = slow
+	static const float RESET_SPEED_FREELOOK = 0.1;            // how fast the camera will reset from freelook angles to default. 0.1 = fast | 0.4 = slow
+	static const float RESET_SPEED_DEADZONE = 0.3;            // how fast the camera will reset from deadzone angles to default. 0.1 = fast | 0.4 = slow
+	static const float RESET_SPEED_DEADZONE_WEAPON = 0.3;     // how fast the camera will reset from weapon deadzone angles to default. 0.1 = fast | 0.4 = slow
+	  
+	static const float INERTIA_SMOOTHNESS = 0.2;                       // how smooths the hands will "lag" behind the camera. Recommended 0.15 > x > 0.40
+	static const float INERTIA_MULTIPLIER_BASE = 2;                    // Base multiplier of the amount of inertia to be applied
+	static const float INERTIA_MULTIPLIER_WEAPON_WEIGHT = 3 * 0.0001;  // Multiplier of the amount of inertia to be applied based on weapon weight (example: 5 * 0.0001 means 5 every kilogram)
+	static const float INERTIA_MULTIPLIER_HIPFIRE = 5;                 // Multiplier of the amount of inertia to be applied when the player is in hipfire
+	
+	static const float INERTIA_MULTIPLIER_STANDING = 1.0;              // Multiplier of the amount of inertia to be applied when the player is NOT MOVING
+	static const float INERTIA_MULTIPLIER_WALKING = 1.5;               // Multiplier of the amount of inertia to be applied when the player is WALKING
+	static const float INERTIA_MULTIPLIER_JOGGING = 2.0;               // Multiplier of the amount of inertia to be applied when the player is JOGGING
+	
+	static const float INERTIA_MULTIPLIER_ERECT = 1;                   // Multiplier of the amount of inertia to be applied when the player is ERECT
+	static const float INERTIA_MULTIPLIER_CROUCHED = 0.5;              // Multiplier of the amount of inertia to be applied when the player is CROUCHED
+	static const float INERTIA_MULTIPLIER_PRONE = 0.9;                 // Multiplier of the amount of inertia to be applied when the player is PRONE
 	
 	/////////////////////////////////////////////////////////////
 	// DYNAMIC CROSSHAIR
-	static const float CROSSHAIR_PRECISION = 50;            // How much precise the crosshair placement will be. (metres)
-	static const float CROSSHAIR_SMOOTHNESS = 0.05;         // How much smooth the crosshair placement will be.
+	static const float CROSSHAIR_PRECISION = 50;              // How much precise the crosshair placement will be. (metres)
+	static const float CROSSHAIR_SMOOTHNESS = 0.05;           // How much smooth the crosshair placement will be.
 }
--- scripts/4_World/module_gunplay/DayZPlayerImplementAiming.c
+++ scripts/4_World/module_gunplay/DayZPlayerImplementAiming.c
@@ -16,31 +16,44 @@ modded class DayZPlayerImplementAiming{
 	
 	override bool ProcessAimFilters(float pDt, SDayZPlayerAimingModel pModel, int stance_index){
 		bool result = super.ProcessAimFilters(pDt, pModel, stance_index);
-		//pModel.m_fAimXHandsOffset *= 5;
-		//pModel.m_fAimYHandsOffset *= 5;
-		if(GunplayConstants.USE_LEGACY_MODE){
-			float aimChangeX = getAimChangeDegree()[0];
-			float aimChangeY = getAimChangeDegree()[1];
-		
-			Weapon_Base weapon = Weapon_Base.Cast(m_PlayerPb.GetItemInHands());
-			float inertiaMultiplier = computeDragMultiplier(weapon);
-			
-			aimChangeX *= inertiaMultiplier ;
-			aimChangeY *= inertiaMultiplier ;
 
-			pModel.m_fAimXHandsOffset = Math.SmoothCD(pModel.m_fAimXHandsOffset, pModel.m_fAimXHandsOffset - aimChangeX, m_inertiaXVel, GunplayConstants.INERTIA_SMOOTHNESS, 1000, pDt);
-			pModel.m_fAimYHandsOffset = Math.SmoothCD(pModel.m_fAimYHandsOffset, pModel.m_fAimYHandsOffset - aimChangeY, m_inertiaYVel, GunplayConstants.INERTIA_SMOOTHNESS, 1000, pDt);
-			
-			updateSCrosshair(weapon, pDt);
+		Weapon_Base weapon = Weapon_Base.Cast(m_PlayerPb.GetItemInHands());
+		updateSCrosshair(weapon, pDt);
+		
+		if(GunplayConstants.USE_WEAPON_INERTIA){
+			applyWeaponInertia(pModel);
+		}		
+	
+		if(!GunplayConstants.CAMERA_FOLLOWS_BREATHING_SWAY){
+			updateSwayOffset(pModel);
 		}
-		
-		
-		DayZPlayerCameraIronsights.m_weaponDeadzoneX = pModel.m_fAimXHandsOffset ;
-		DayZPlayerCameraIronsights.m_weaponDeadzoneY = pModel.m_fAimYHandsOffset ;
 		
 		return result;
 	}
 	
+	
+	
+	
+	
+	/**
+	*	@brief Apply the inertia to the given Aiming Model 
+	*	 @param pModel \p SDayZPlayerAimingModel - Player aiming model
+	*/
+	protected void applyWeaponInertia(SDayZPlayerAimingModel pModel){
+		float inertiaMultiplier = computeInertiaMultiplier(weapon);
+			
+		float aimChangeX = getAimChangeDegree()[0] * inertiaMultiplier;
+		float aimChangeY = getAimChangeDegree()[1] * inertiaMultiplier;
+	
+		pModel.m_fAimXHandsOffset = Math.SmoothCD(pModel.m_fAimXHandsOffset, pModel.m_fAimXHandsOffset - aimChangeX, m_inertiaXVel, GunplayConstants.INERTIA_SMOOTHNESS, 1000, pDt);
+		pModel.m_fAimYHandsOffset = Math.SmoothCD(pModel.m_fAimYHandsOffset, pModel.m_fAimYHandsOffset - aimChangeY, m_inertiaYVel, GunplayConstants.INERTIA_SMOOTHNESS, 1000, pDt);
+	}
+	
+	
+	/**
+	*	@brief Update the crosshair position on the screen vector and its visibility
+	*	 @param weapon \p Weapon_Base - Weapon used to get the direction of the raycast
+	*/
 	protected void updateSCrosshair(Weapon_Base weapon, float pDt){
 		if(!GetGame().GetMission() || m_PlayerDpi.IsInIronsights() || m_PlayerDpi.IsInOptics()){
 			m_isSCrosshairVisible = false;
@@ -62,6 +75,10 @@ modded class DayZPlayerImplementAiming{
 		}
 	}
 	
+	/**
+	*	@brief Get the user input aim change
+	*	 @return vector - Aim change of the player (x, y, 0)
+	*/
 	protected vector getAimChangeDegree(){
 		vector aimChange = "0 0 0";
 		aimChange[0] = m_PlayerDpi.GetInputController().GetAimChange()[0] * Math.RAD2DEG;
@@ -69,20 +86,58 @@ modded class DayZPlayerImplementAiming{
 		return aimChange;
 	}
 	
-	protected float computeDragMultiplier(Weapon_Base weapon){
+	/**
+	*	@brief Compute the amount of inertia
+	*	 @param weapon \p Weapon_Base - Weapon used for the computation
+	*	 @return float - inertia amount
+	*/
+	protected float computeInertiaMultiplier(Weapon_Base weapon){
 		float inertiaMultiplier = GunplayConstants.INERTIA_MULTIPLIER_BASE;
-		if(weapon) inertiaMultiplier = weapon.GetWeight() / 1000;
+
+		////////////////////////
+		// STANCE 
+		inertiaMultiplier *= getStanceInertiaMultiplier();
+		
+		////////////////////////
+		// MOVEMENT
+		switch(m_PlayerDpi.m_MovementState.m_iMovement){ 
+			case 0:	//standing
+				inertiaMultiplier *= GunplayConstants.INERTIA_MULTIPLIER_STANDING;
+				break;
+
+			case 1:	//walking
+				inertiaMultiplier *= GunplayConstants.INERTIA_MULTIPLIER_WALKING;
+				break;
+
+			case 2:	//jogging
+				inertiaMultiplier *= GunplayConstants.INERTIA_MULTIPLIER_JOGGING;
+				break;
+			
+			default: //flying? lol
+				inertiaMultiplier *= 1;
+		}
+		
+		////////////////////////
+		// WEAPON WEIGHT
+		inertiaMultiplier *= weapon.GetWeight() * GunplayConstants.INERTIA_MULTIPLIER_WEAPON_WEIGHT;
+		
+		////////////////////////
+		// HIPFIRE 
 		if(!m_PlayerDpi.IsInOptics() && !m_PlayerDpi.IsInIronsights()){
 			inertiaMultiplier *= GunplayConstants.INERTIA_MULTIPLIER_HIPFIRE;
 		}
 		
-		inertiaMultiplier *= getStanceInertiaMultiplier();
+		
 		
 		
 		return inertiaMultiplier;
 	}
+		
 	
-	
+	/**
+	*	@brief Get the inertia multiplier based on the player stance
+	*	 @return float - inertia multiplier
+	*/
 	protected float getStanceInertiaMultiplier(){
 		if(m_PlayerPb.IsPlayerInStance(DayZPlayerConstants.STANCEMASK_RAISEDERECT | DayZPlayerConstants.STANCEMASK_ERECT)){
 			return GunplayConstants.INERTIA_MULTIPLIER_ERECT;
@@ -93,6 +148,15 @@ modded class DayZPlayerImplementAiming{
 		} 
 		
 		return 1;
+	}
+	
+	/**
+	*	@brief Untie the camera from the weapon sway
+	*	 @param pModel \p SDayZPlayerAimingModel - Player aiming model
+	*/
+	protected void updateSwayOffset(SDayZPlayerAimingModel pModel){
+		DayZPlayerCameraIronsights.m_weaponDeadzoneX = pModel.m_fAimXHandsOffset;
+		DayZPlayerCameraIronsights.m_weaponDeadzoneY = pModel.m_fAimYHandsOffset;
 	}
 	
 	
