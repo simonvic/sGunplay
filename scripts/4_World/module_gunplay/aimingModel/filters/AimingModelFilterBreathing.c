d1188fa1593d3931c4bd3522683edcad8400f108
simonvic <simonvic.dev@gmail.com>
2021-05-31 15:00:34 +0200

    Reworked sway filter

--- scripts/4_World/module_gunplay/aimingModel/filters/AimingModelFilterBreathing.c
+++ scripts/4_World/module_gunplay/aimingModel/filters/AimingModelFilterBreathing.c
@@ -2,17 +2,34 @@ class AimingModelFilterBreathing : AimingModelFilterBase{
 	
 	protected float m_time;
 	protected vector m_breathingOffset;
+	protected float m_velX[1];
+	protected float m_velY[1];
 	
 	override void onUpdate(float pDt, SDayZPlayerAimingModel pModel, int stanceIndex){
-		m_time += pDt * getSpeed();		
-		computeBreathingPattern(calculateWeight(stanceIndex));
-		pModel.m_fAimXHandsOffset += m_breathingOffset[0];
-		pModel.m_fAimYHandsOffset += m_breathingOffset[1];
+		m_time += pDt * getSpeed();
+		
+		computeBreathingPattern(calculateWeight(stanceIndex), GunplayConstants.SWAY_FREQUENCY, GunplayConstants.SWAY_AMPLITUDE);
+		
+		pModel.m_fAimXHandsOffset += Math.SmoothCD(
+			pModel.m_fAimXHandsOffset,
+			pModel.m_fAimXHandsOffset + m_breathingOffset[0],
+			m_velX,
+			0.1, 1000, pDt);
+		
+		pModel.m_fAimYHandsOffset += Math.SmoothCD(
+			pModel.m_fAimYHandsOffset,
+			pModel.m_fAimYHandsOffset + m_breathingOffset[1],
+			m_velY,
+			0.1, 1000, pDt);
+		
+		
+		
+		
 	}
 	
-	protected void computeBreathingPattern(float weight){
-		m_breathingOffset[0] = Math.Sin(m_time + GunplayConstants.SWAY_FREQUENCY[0]) * GunplayConstants.SWAY_AMPLITUDE[0] * weight;
-		m_breathingOffset[1] = Math.Sin(m_time + GunplayConstants.SWAY_FREQUENCY[1]) * GunplayConstants.SWAY_AMPLITUDE[1] * weight;
+	protected void computeBreathingPattern(float weight, float frequency[2], float amplitude[2]){
+		m_breathingOffset[0] = m_breathingOffset[0] / 2 + Math.Sin(m_time + frequency[0]) * amplitude[0] * weight;
+		m_breathingOffset[1] = m_breathingOffset[1] / 2 + Math.Sin(m_time + frequency[1]) * amplitude[1] * weight;
 	}
 	
 	protected float getSpeed(){
