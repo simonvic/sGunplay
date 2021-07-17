ea2fcaf2a2ba752620163d6e09d041ba7932dd8b
simonvic <simonvic.dev@gmail.com>
2021-07-17 16:14:24 +0200

    focusing, increased stamina drain exponent + slight less drain

--- scripts/3_Game/GameConstants.c
+++ scripts/3_Game/GameConstants.c
@@ -1,10 +1,10 @@
 modded class GameConstants{
 
-	const float	STAMINA_DRAIN_HOLD_BREATH_START = 0.1; //in units (how much holding breath depletes stamina at the start)
-	const float	STAMINA_DRAIN_HOLD_BREATH_END = 0.2; //in units (how much holding breath depletes stamina at the end)
+	const float	STAMINA_DRAIN_HOLD_BREATH_START = 0.075; //in units (how much holding breath depletes stamina at the start)
+	const float	STAMINA_DRAIN_HOLD_BREATH_END = 0.15; //in units (how much holding breath depletes stamina at the end)
 
-	const float STAMINA_DRAIN_HOLD_BREATH_DURATION = 10.0; //in seconds, time it takes to increase stamina drain from STAMINA_DRAIN_HOLD_BREATH_START to STAMINA_DRAIN_HOLD_BREATH_END
-	const float	STAMINA_DRAIN_HOLD_BREATH_EXPONENT = 2.0; //holding breath exponent
+	const float STAMINA_DRAIN_HOLD_BREATH_DURATION = 7.5; //in seconds, time it takes to increase stamina drain from STAMINA_DRAIN_HOLD_BREATH_START to STAMINA_DRAIN_HOLD_BREATH_END
+	const float	STAMINA_DRAIN_HOLD_BREATH_EXPONENT = 2.5; //holding breath exponent
 
 	
 }
