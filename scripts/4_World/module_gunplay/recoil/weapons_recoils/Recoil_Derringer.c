45324827c697f8a15cc26cd7119e3aff9b895d26
simonvic <simonvic.dev@gmail.com>
2022-05-19 19:43:30 +0200

    Add Derringer and M79 recoil

---
+++ scripts/4_World/module_gunplay/recoil/weapons_recoils/Recoil_Derringer.c
@@ -0,0 +1,26 @@
+modded class DerringerRecoil {
+	
+	override void Init() {
+		m_handsMinHorizontalRecoil = -0.10;
+		m_handsMaxHorizontalRecoil = 0.25;
+		
+		m_handsMinVerticalRecoil = 2.035;
+		m_handsMaxVerticalRecoil = 2.717;
+		m_handsRecoilsteps = 2;
+		
+		m_HandsOffsetRelativeTime = 2.8;
+		
+		m_relativeReloadTime = 2.5;
+		
+		m_MouseOffsetRangeMin = 60;
+		m_MouseOffsetRangeMax = 100;
+		m_MouseOffsetDistance = 3.4;
+		m_MouseOffsetRelativeTime = 0.2;
+	
+		m_CamOffsetDistance = 0.08;
+		m_CamOffsetRelativeTime = 1;
+		
+		createHandRecoilPoints();
+	}
+	
+}
