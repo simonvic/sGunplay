f275fcd891094f3bfe5589360b39dca97a633af0
simonvic <simonvic.dev@gmail.com>
2022-08-13 18:06:10 +0200

    fix: aiming model filter clamps

--- scripts/4_World/sUDE/aimingModel/filters/AimingModelFilterClamps.c
+++ scripts/4_World/sUDE/aimingModel/filters/AimingModelFilterClamps.c
@@ -1,7 +1,7 @@
 class AimingModelFilterClamps : AimingModelFilterBase {
 	
 	override void onUpdate(float pDt, SDayZPlayerAimingModel pModel, int stanceIndex) {		
-		pModel.m_fAimYHandsOffset = Math.Clamp(pModel.m_fAimYHandsOffset, -160, 160);
+		pModel.m_fAimXHandsOffset = Math.Clamp(pModel.m_fAimXHandsOffset, -160, 160);
 		pModel.m_fAimYHandsOffset = Math.Clamp(pModel.m_fAimYHandsOffset, -85 - pModel.m_fCurrentAimY, 85 - pModel.m_fCurrentAimY);
 	}
 	
