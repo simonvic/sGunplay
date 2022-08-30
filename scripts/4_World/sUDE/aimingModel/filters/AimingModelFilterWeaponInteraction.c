b2428ce06c5fae03b9f44ae1712e5f456f49d888
simonvic <simonvic.dev@gmail.com>
2022-08-30 17:33:22 +0200

    fix: aiming model filter weapon interaction
    - changed hand and camera animation
    - the hand and camera animation only plays when changing firemode
    - fixed multiple wrong interaction triggers when changing optics or
      weapon

--- scripts/4_World/sUDE/aimingModel/filters/AimingModelFilterWeaponInteraction.c
+++ scripts/4_World/sUDE/aimingModel/filters/AimingModelFilterWeaponInteraction.c
@@ -1,128 +1,135 @@
 class AimingModelFilterWeaponInteraction : AimingModelFilterBase {
 	
 	protected Weapon_Base m_prevWeapon;
+	protected ItemOptics m_prevOptic;
 	protected int m_prevFireMode;
 	protected int m_prevStepZeroing;
 	protected int m_prevStepZoom;
 	
-	protected bool m_fireModeChanged;
-	protected bool m_zeroingChanged;
-	protected bool m_zoomChanged;
-	protected bool m_weaponChanged;
-	
-	protected float m_velX[1];
-	protected float m_velY[1];
-	
-	protected float m_time;
-	protected bool reverse;
-	protected bool done = true;
+	protected float m_animationTime;
+	protected bool m_playHandAnimation;
 	
 	override bool isActive() {
 		return GunplayConstants.AIMING_MODEL_USE_FILTER_WEAPON_INTERACTION;
 	}
 	
 	override void onUpdate(float pDt, SDayZPlayerAimingModel pModel, int stanceIndex) {
-		m_fireModeChanged = false;
-		m_zeroingChanged = false;
-		m_zoomChanged = false;
-		m_weaponChanged = false;
-		float offset[] = {0, 0};
-		
-		updateState();
-		
-		if (!m_weaponChanged && (m_fireModeChanged || m_zeroingChanged || m_zoomChanged)) {
-			offset = GunplayConstants.AIMING_MODEL_USE_FILTER_WEAPON_INTERACTION_OFFSETS;
-			offset = {-40, 100};
-			done = false;
-			if (GetGame().IsClient() || !GetGame().IsMultiplayer()) {
-				playSounds(getSoundSet());
-			}
-		}
-		
-		
-		if (m_time >= 1) reverse = true;
-		if (m_time < 0) {
-			reverse = false;
-			done = true;
-		}
-		if (done) {
-			m_time = 0;
+
+		if (getWeapon() != m_prevWeapon) {
+			m_prevWeapon = getWeapon();
+			updateStateOptic();
 			return;
 		}
-		if (reverse) {
-			m_time -= pDt * 3;
-		} else {
-			m_time += pDt * 5;
+		
+		updateStateOptic();
+		
+		if (updateStateZoom()) {
+			playSounds(GunplayConstants.SOUND_CHANGE_ZOOM);
 		}
 		
-		
-		pModel.m_fAimXHandsOffset += Easing.EaseInSine(m_time) * 2;
-		pModel.m_fAimYHandsOffset -= Easing.EaseOutSine(m_time) * 2;
-		
-		if (GetGame().IsClient()) {
-			pModel.m_fAimXCamOffset -= Easing.EaseInOutSine(m_time) * 3;
-			pModel.m_fCamPosOffsetZ += Easing.EaseInOutSine(m_time) * 0.025;
+		if (updateStateZeroing()) {
+			playSounds(GunplayConstants.SOUND_CHANGE_ZEROING);
 		}
-
-
+		
+		if (updateStateFiremode()) {
+			m_playHandAnimation = true;
+		}
+		
+		if (m_playHandAnimation) {
+			playHandAnimation(pDt, pModel);
+		}
 	}
 	
 	/**
-	*	@brief Check if a state has changed. Can't use HumanInputController
+	*	@brief Update the current optic
+	*	@return true if it has changed, false otherwise
 	*/
-	protected void updateState() {
-		Weapon_Base weapon = getWeapon();
-		
-		if (weapon != m_prevWeapon) {
-			m_weaponChanged = true;
-			m_prevWeapon = weapon;
+	protected bool updateStateOptic() {
+		ItemOptics currOptic = getWeapon().GetAttachedOptics();
+		if (currOptic != m_prevOptic) {
+			m_prevOptic = currOptic;
+			updateStateZoom();
+			updateStateZeroing();
+			return true;
 		}
-		
+		return false;
+	}
+	
+	/**
+	*	@brief Update the current zoom state
+	*	@return true if it has changed, false otherwise
+	*/
+	protected bool updateStateZoom() {
+		if (!m_prevOptic) return false;
+		int currStepZoom = m_prevOptic.GetStepFOVIndex();
+		if (currStepZoom != m_prevStepZoom) {
+			m_prevStepZoom = currStepZoom;
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	*	@brief Update the current zeroing state
+	*	@return true if it has changed, false otherwise
+	*/
+	protected bool updateStateZeroing() {
+		int currStepZeroing = m_prevWeapon.GetStepZeroing();
+		if (currStepZeroing != m_prevStepZeroing) {
+			m_prevStepZeroing = currStepZeroing;
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	*	@brief Update the current firemode state
+	*	@return true if it has changed, false otherwise
+	*/
+	protected bool updateStateFiremode() {
 		//@fixme shooting in double shot firemode triggers a firemodechange
-		int fireMode = weapon.GetCurrentMode(weapon.GetCurrentMuzzle());
-		if (fireMode != m_prevFireMode) {
-			m_prevFireMode = fireMode;
-			m_fireModeChanged = true;
+		int currFireMode = m_prevWeapon.GetCurrentMode(m_prevWeapon.GetCurrentMuzzle());
+		if (currFireMode != m_prevFireMode) {
+			m_prevFireMode = currFireMode;
+			return true;
 		}
-		
-		int stepZeroing = weapon.GetStepZeroing();
-		if (stepZeroing != m_prevStepZeroing) {
-			m_prevStepZeroing = stepZeroing;
-			m_zeroingChanged = true;
-		}
-		
-		ItemOptics optic = weapon.GetAttachedOptics();
-		if (optic) {
-			int stepZoom = optic.GetStepFOVIndex();
-			if (stepZoom != m_prevStepZoom) {
-				m_prevStepZoom = stepZoom;
-				m_zoomChanged = true;
+		return false;
+	}
+	
+	/**
+	*	@brief Animate hands and camera
+	*/
+	protected void playHandAnimation(float pDt, SDayZPlayerAimingModel pModel) {
+		if (m_animationTime > 1) {
+			m_playHandAnimation = false;
+			m_animationTime = 0;
+		} else {
+			m_animationTime += pDt * 2;
+			float loopedTime = 1 - Math.AbsFloat(m_animationTime - 0.5) * 2;
+			
+			pModel.m_fAimXHandsOffset += Easing.EaseInOutSine(loopedTime) * 0.3;
+			pModel.m_fAimYHandsOffset -= Easing.EaseInSine(loopedTime) * 0.5;
+			
+			if (GetGame().IsClient()) {
+				pModel.m_fAimXCamOffset -= Easing.EaseInOutBack(loopedTime, 0.69) * 0.2;
+				pModel.m_fAimYCamOffset -= Easing.EaseInSine(loopedTime) * 0.1;
+				pModel.m_fCamPosOffsetZ += Easing.EaseInOutSine(loopedTime) * 0.005;
 			}
 		}
 	}
 	
+
 	/**
 	*	@brief Play sounds only on client
-	*	 @param soundSet \p string - classname of soundset (from DZ/sounds/hpp/config.cpp) to be played
+	*	@param soundSet \p string - classname of soundset (from DZ/sounds/hpp/config.cpp) to be played
 	*/
 	protected void playSounds(string soundSet) {
+		if (!GetGame().IsClient()) return;
 		if (soundSet != string.Empty) {
-			SEffectManager.PlaySoundOnObject( soundSet , getWeapon()).SetSoundAutodestroy( true );
+			SEffectManager.PlaySoundOnObject(soundSet, getWeapon()).SetSoundAutodestroy(true);
 		}
 	}
-	
-	/**
-	*	@brief Get soundset to play based on performed interaction
-	*	 @return string - soundset classname
-	*/
-	protected string getSoundSet() {
-		if (m_zeroingChanged) {
-			return GunplayConstants.SOUND_CHANGE_ZEROING;
-		} else if (m_zoomChanged) {
-			return GunplayConstants.SOUND_CHANGE_ZOOM;
-		}
-		return string.Empty;
-	}
+
 	
 	
 }
