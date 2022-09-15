e0ebdaa4d8582d988cd5dda12d92841521a42be7
simonvic <simonvic.dev@gmail.com>
2022-09-15 11:40:59 +0200

    fix: possibly definitive solution to weight desync

--- scripts/4_World/sUDE/PlayerBase.c
+++ scripts/4_World/sUDE/PlayerBase.c
@@ -5,6 +5,27 @@ modded class PlayerBase {
 		m_recoilControl = new RecoilControl(this);
 	}
 	
+	override void Init() {
+		super.Init();
+		RegisterNetSyncVariableFloat("m_Weight");
+	}
+	
+	override void UpdateWeight(WeightUpdateType updateType = WeightUpdateType.FULL, float weightAdjustment = 0) {
+		if (GetGame().IsClient()) return;
+		super.UpdateWeight(updateType, weightAdjustment);
+		
+		
+		/* mod friendly version
+		if (GetGame().IsClient()) {
+			float temp = m_Weight;
+			super.UpdateWeight(updateType, weightAdjustment);
+			m_Weight = temp;
+		} else {
+			super.UpdateWeight(updateType, weightAdjustment);
+		}
+		*/
+	}
+	
 	RecoilControl getRecoilControl() {
 		return m_recoilControl;
 	}
