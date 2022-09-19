8ec75f6baf13c07ac44b37bd7f9ef00f4cdaea18
simonvic <simonvic.dev@gmail.com>
2022-09-19 17:53:50 +0200

    feat: third iteration of aiming model inertia
    - the benefits from the second iteration carried over to the initial
      design, resulting in better weight feeling while not losing
      responsiveness

--- scripts/4_World/sUDE/aimingModel/filters/AimingModelFilterInertia.c
+++ scripts/4_World/sUDE/aimingModel/filters/AimingModelFilterInertia.c
@@ -14,40 +14,82 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	}
 	
 	override void onUpdate(float pDt, SDayZPlayerAimingModel pModel, int stanceIndex) {
+		auto dui = SDebugUI.of("inertia");
+		dui.begin();
+		dui.pos("0.7 0").size("512px").window();
+		float multiplier = computeInertiaMultiplier();
 		
 		//////////////////////////
 		// ACCELERATION
-		m_accel = getAimingModel().getAimChangeDegree();
+		m_accel = getAimingModel().getAimChangeDegree() * multiplier;
 		m_vel += m_accel;
 		m_vel[0] = Math.Clamp(m_vel[0], GunplayConstants.INERTIA_VELOCITY_LIMIT[1], GunplayConstants.INERTIA_VELOCITY_LIMIT[2]);
 		m_vel[1] = Math.Clamp(m_vel[1], GunplayConstants.INERTIA_VELOCITY_LIMIT[3], GunplayConstants.INERTIA_VELOCITY_LIMIT[0]);
+		float resetSpeed = 0.20;
+		float accelSpeed = 0.05;
+		float inertiaSpeedAcceleration[2] = {accelSpeed, accelSpeed};
+		float inertiaSpeedReset[2] = {resetSpeed, resetSpeed};
 		
-		float inertiaSpeedAcceleration[2];
-		float inertiaSpeedReset[2];
+		dui.table({
+			{"",               "inertia multipliers",                    ""}
+			{"base",           "0","x"}
+			{"stance",         ""+getInertiaMultiplierStance(),          "x"}
+			{"movement",       ""+getInertiaMultiplierMovement(),        "x"}
+			{"wep weight",     ""+getInertiaMultiplierWeapon(),          "x"}
+			{"wep length",     ""+getInertiaMultiplierWeaponLength(),    "x"}
+			{"inventory",      ""+getInertiaMultiplierInventoryWeight(), "x"}
+			{"hipfire",        ""+getInertiaMultiplierHipfire(),         "="}
+			{"--------------------"}
+			{"total",          ""+multiplier,                     " "}
+		});
+		dui.spacer();
+		dui.table({
+			{"Multiplier" , ""+multiplier}
+			{"accelSpeed" , ""+accelSpeed}
+			{"resetSpeed" , ""+resetSpeed}
+			{"m_accel" , ""+m_accel}
+			{"wepLength" , ""+getWeapon().getWeaponLength()}
+			{"m_vel" , ""+m_vel[0], ""+m_vel[1]}
+		});
 		
-		if (getPlayer().IsInOptics() || getPlayer().IsInIronsights()) {
-			inertiaSpeedAcceleration = GunplayConstants.INERTIA_SPEED_ACCELERATION;
-			inertiaSpeedReset = GunplayConstants.INERTIA_SPEED_RESET;
+		vector initial;
+		initial[0] = pModel.m_fAimXHandsOffset;
+		initial[1] = pModel.m_fAimYHandsOffset;
+		
+		if (false) {
+			pModel.m_fAimXHandsOffset -= m_vel[0];
+			pModel.m_fAimYHandsOffset -= m_vel[1];
 		} else {
-			inertiaSpeedAcceleration = GunplayConstants.INERTIA_SPEED_ACCELERATION_HIPFIRE;
-			inertiaSpeedReset = GunplayConstants.INERTIA_SPEED_RESET_HIPFIRE;
+			pModel.m_fAimXHandsOffset = Math.SmoothCD(
+				pModel.m_fAimXHandsOffset,
+				pModel.m_fAimXHandsOffset - m_vel[0],
+				m_currInertiaVelX,
+				inertiaSpeedAcceleration[0], 1000, pDt);
+			
+			pModel.m_fAimYHandsOffset = Math.SmoothCD(
+				pModel.m_fAimYHandsOffset,
+				pModel.m_fAimYHandsOffset - m_vel[1],
+				m_currInertiaVelY,
+				inertiaSpeedAcceleration[1], 1000, pDt);
 		}
+			
+		vector final;
+		final[0] = pModel.m_fAimXHandsOffset;
+		final[1] = pModel.m_fAimYHandsOffset;
+		vector delta = Vector(initial[0] - final[0], initial[1] - final[1], 0);
 		
-		float multiplier = computeInertiaMultiplier();
 		
-		pModel.m_fAimXHandsOffset += Math.SmoothCD(
-			0,
-			m_vel[0] * multiplier,
-			m_currInertiaVelX,
-			inertiaSpeedAcceleration[0], 1000, pDt);
 		
-		pModel.m_fAimYHandsOffset += Math.SmoothCD(
-			0,
-			m_vel[1] * multiplier,
-			m_currInertiaVelY,
-			inertiaSpeedAcceleration[1], 1000, pDt);
+		/*
+		dui.table({
+			{"initial", ""+initial}
+			{"final", ""+final}
+		});
+		dui.plotlive("m_velX", Math.AbsFloat(m_vel[0]), GunplayConstants.INERTIA_VELOCITY_LIMIT[1], GunplayConstants.INERTIA_VELOCITY_LIMIT[2]);
+		dui.plotlive("deltaX", delta[0], -10, 10);
+		dui.plotlive("deltaY", delta[1], -10, 10);
+		*/
 		
-				
 		//////////////////////////
 		// DECELERATION		
 		m_vel[0] = Math.SmoothCD(
@@ -63,11 +105,11 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 			inertiaSpeedReset[1], 1000, pDt);
 		
 		if (GetGame().IsClient()) {
-			pModel.m_fAimXCamOffset -= m_vel[0] * 0.01;
-			pModel.m_fAimYCamOffset -= m_vel[1] * 0.01;
+			pModel.m_fAimXCamOffset += m_vel[0] * 0.05;
+			pModel.m_fAimYCamOffset += m_vel[1] * 0.05;
 		}
 		
-		
+		dui.end();
 	}
 	
 	/**
@@ -75,35 +117,16 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*	 @param weapon \p Weapon_Base - Weapon used for the computation
 	*	 @return float - inertia amount
 	*/
-	protected float computeInertiaMultiplier() {
-		//if (getPlayer().IsHoldingBreath()) return 0.9;
-		float inertiaMultiplier = GunplayConstants.INERTIA_MULTIPLIER_BASE;
-
-		////////////////////////
-		// STANCE 
+	protected float computeInertiaMultiplier() {		
+		float inertiaMultiplier = 1.0;
 		inertiaMultiplier *= getInertiaMultiplierStance();
-		
-		////////////////////////
-		// MOVEMENT
 		inertiaMultiplier *= getInertiaMultiplierMovement();
-		
-		////////////////////////
-		// WEAPON WEIGHT
 		inertiaMultiplier *= getInertiaMultiplierWeapon();
-		
-		////////////////////////
-		// WEAPON BARREL LENGTH
 		inertiaMultiplier *= getInertiaMultiplierWeaponLength();
-		
-		////////////////////////
-		// PLAYER INVENTORY WEIGHT
 		inertiaMultiplier *= getInertiaMultiplierInventoryWeight();
-				
-		////////////////////////
-		// HIPFIRE 
 		inertiaMultiplier *= getInertiaMultiplierHipfire();
-		
-		return Math.Clamp(inertiaMultiplier, GunplayConstants.INERTIA_MIN_MULTIPLIER, GunplayConstants.INERTIA_MAX_MULTIPLIER);
+		inertiaMultiplier  = Math.Clamp(inertiaMultiplier, 0, 1);		
+		return inertiaMultiplier;
 	}
 		
 	
@@ -113,13 +136,12 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*/
 	protected float getInertiaMultiplierStance() {
 		if (getPlayer().IsPlayerInStance(DayZPlayerConstants.STANCEMASK_RAISEDERECT | DayZPlayerConstants.STANCEMASK_ERECT)) {
-			return GunplayConstants.INERTIA_MULTIPLIER_ERECT;
+			return 1;
 		} else if (getPlayer().IsPlayerInStance(DayZPlayerConstants.STANCEMASK_RAISEDCROUCH | DayZPlayerConstants.STANCEMASK_CROUCH)) {
-			return GunplayConstants.INERTIA_MULTIPLIER_CROUCHED;
+			return 0.30;
 		} else if (getPlayer().IsPlayerInStance(DayZPlayerConstants.STANCEMASK_RAISEDPRONE | DayZPlayerConstants.STANCEMASK_PRONE)) {
-			return GunplayConstants.INERTIA_MULTIPLIER_PRONE;
-		} 
-		
+			return 0.25;
+		}
 		return 1;
 	}
 	
@@ -129,9 +151,9 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*/
 	protected float getInertiaMultiplierMovement() {
 		switch (getPlayer().m_MovementState.m_iMovement) { 
-			case 0:	return GunplayConstants.INERTIA_MULTIPLIER_STANDING;
-			case 1: return GunplayConstants.INERTIA_MULTIPLIER_WALKING;
-			case 2:	return GunplayConstants.INERTIA_MULTIPLIER_JOGGING;
+			case 0:	return 1;
+			case 1: return 1.25;
+			case 2:	return 1.50;
 		}
 		return 1;
 	}
@@ -142,10 +164,10 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*/
 	protected float getInertiaMultiplierWeapon() {
 		int w = getWeapon().GetWeight();
-		if (w > 5000) return 2.00;
-		if (w > 3000) return 1.75;
-		if (w > 2000) return 1.50;
-		if (w > 1000) return 1.25;
+		if (w > 6000) return 2.00;
+		if (w > 4000) return 1.75;
+		if (w > 2000) return 1.25;
+		if (w > 1000) return 1.10;
 		return 1;
 	}
 	
@@ -154,7 +176,7 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*	 @return float - inertia multiplier
 	*/
 	protected float getInertiaMultiplierWeaponLength() {
-		return getWeapon().getWeaponLength() * GunplayConstants.INERTIA_MULTIPLIER_WEAPON_LENGTH_WEIGHT;
+		return getWeapon().getWeaponLength() * 0.25;
 	}
 	
 	/**
@@ -163,7 +185,7 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*/
 	protected float getInertiaMultiplierInventoryWeight() {
 		int w = getPlayer().GetWeight();
-		if (w > 40000) return 2.00;
+		if (w > 40000) return 1.90;
 		if (w > 20000) return 1.75;
 		if (w > 10000) return 1.50;
 		if (w > 5000)  return 1.25;
@@ -175,9 +197,6 @@ class AimingModelFilterInertia : AimingModelFilterBase {
 	*	 @return float - inertia multiplier
 	*/
 	protected float getInertiaMultiplierHipfire() {
-		if (!getPlayer().IsInOptics() && !getPlayer().IsInIronsights()) {
-			return GunplayConstants.INERTIA_MULTIPLIER_HIPFIRE;
-		}
 		return 1;
 	}
 
