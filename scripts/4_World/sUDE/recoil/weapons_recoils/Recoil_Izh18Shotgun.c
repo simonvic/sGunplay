13c73af2b11b3df0a924282e57f6bad53cbe70e2
simonvic <simonvic.dev@gmail.com>
2022-10-21 16:29:23 +0200

    feat: add Izh18 shotgun recoil

---
+++ scripts/4_World/sUDE/recoil/weapons_recoils/Recoil_Izh18Shotgun.c
@@ -0,0 +1,17 @@
+modded class Izh18ShotgunRecoil {
+	override void initRecoilParameters() {
+		handsRanges        = { -1.165, 3.285,  7.760,  9.255};	
+		handsAccumSpeed    = 0.955;
+		handsResetSpeed    = 0.700;
+
+		misalignIntensity  = {0.250, 0.250};
+		misalignAccumSpeed = 0.955;
+		misalignResetSpeed = 0.500;
+	
+		mouseRanges        = { 0.175, 0.250,  1.750,  2.775};
+		mouseTime          = 0.075;
+	
+		kick               = 0.045;
+		kickResetTime      = 3.250;
+	}
+}
