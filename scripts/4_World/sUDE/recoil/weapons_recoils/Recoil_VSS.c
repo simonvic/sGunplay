5fc85cdd78b10c072f252be7e35cec367c5b8ae7
simonvic <simonvic.dev@gmail.com>
2023-02-12 17:19:27 +0100

    fix: reduce vss/asval recoil

--- scripts/4_World/sUDE/recoil/weapons_recoils/Recoil_VSS.c
+++ scripts/4_World/sUDE/recoil/weapons_recoils/Recoil_VSS.c
@@ -1,15 +1,15 @@
 modded class VSSRecoil {
 	override void initRecoilParameters() {
 		handsRanges        = { -0.165, 0.585,  2.275,  3.050};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.850;
+		handsAccumSpeed    = 0.925;
+		handsResetSpeed    = 0.870;
 
 		misalignIntensity  = {0.125, 0.125};
 		misalignAccumSpeed = 0.955;
 		misalignResetSpeed = 0.650;
 	
-		mouseRanges        = { 0.175, 0.250,  1.550,  2.275};
-		mouseTime          = 0.100;
+		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
+		mouseTime          = 0.150;
 	
 		kick               = 0.020;
 		kickResetTime      = 2.250;
