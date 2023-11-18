2a46f088fb6288c83aeb3c9213b9f4ac63d56c1e
simonvic <simonvic.dev@gmail.com>
2023-11-18 17:23:46 +0100

    refactor: renaming

--- scripts/5_Mission/sUDE/gui/HandsPreview.c
+++ scripts/5_Mission/sUDE/gui/HandsPreview.c
@@ -1,14 +1,14 @@
 modded class HandsPreview {
 	
-	protected Widget m_recoilControlStats;
+	protected Widget m_wRecoilControlStats;
 	
 	void HandsPreview(LayoutHolder parent) {
-		m_recoilControlStats = GetGame().GetWorkspace().CreateWidgets("MyMODS/sGunplay/GUI/layouts/recoil_control_compact.layout", GetMainWidget());
+		m_wRecoilControlStats = GetGame().GetWorkspace().CreateWidgets("MyMODS/sGunplay/GUI/layouts/recoil_control_compact.layout", GetMainWidget());
 	}
 	
 	override void UpdateInterval() {
 		super.UpdateInterval();
 		// TODO(perf): improve this. Just update it when needed
-		ItemManager.GetInstance().updateRecoilControlStats(m_recoilControlStats, m_Item);
+		ItemManager.GetInstance().updateRecoilControlStats(m_wRecoilControlStats, m_Item);
 	}
 }
