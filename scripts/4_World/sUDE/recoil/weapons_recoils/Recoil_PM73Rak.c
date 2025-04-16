5c8c7531a035b09c19da5e9c85dbb45efdcbf747
simonvic <simonvic.dev@gmail.com>
2025-04-16 15:41:30 +0200

    feat: recoils for 1.28 weapons

---
+++ scripts/4_World/sUDE/recoil/weapons_recoils/Recoil_PM73Rak.c
@@ -0,0 +1,19 @@
+#ifndef DAYZ_1_27
+modded class PM73RakRecoil {
+	override void initRecoilParameters() {
+		handsRanges        = {-0.900,  0.900,  2.450,  3.750};	
+		handsAccumSpeed    = 0.975;
+		handsResetSpeed    = 0.730;
+		
+		misalignIntensity  = { 1.750,  0.225};
+		misalignAccumSpeed = 0.975;
+		misalignResetSpeed = 0.850;
+	
+		mouseRanges        = {-0.215,  0.215,  1.250,  1.750};
+		mouseTime          = 0.150;
+	
+		kick               = 0.065;
+		kickResetTime      = 1.450;
+	}
+}
+#endif
